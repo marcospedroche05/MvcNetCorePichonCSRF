9f32008f710fc357594a3058e9ad5b3c2feef92f
marcospedroche05 <marcospedroche62@gmail.com>
2026-02-20 13:51:31 +0100

    pichon terminado

---
+++ MvcNetCorePichonCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCorePichonCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            //SI EL USUARIO NO ESTÁ VALIDADO TODAVIA, LO LLEVAMOS
+            //A DENEGADO
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Productos
+            (string direccion, string[] producto)
+        {
+            if(HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            } else
+            {
+                //LO LLEVAMOS A PEDIDO FINAL, ENVIANDO LA DIRECCCION
+                //Y LOS PRODUCTOS
+                //PARA ENVIAR INFO A OTRO CONTROLLER DEBEMOS 
+                //UTILIZAR TEMPDATA
+                TempData["PRODUCTOS"] = producto;
+                TempData["DIRECCION"] = direccion;
+                return RedirectToAction("PedidoFinal");
+            }
+        }
+
+        public IActionResult PedidoFinal()
+        {
+            //RECUPERAMOS LOS PRODUCTOS
+            string[] productos =
+                TempData["PRODUCTOS"] as string[];
+            ViewData["DIRECCION"] = TempData["DIRECCION"];
+            return View(productos);
+        }
+    }
+}
